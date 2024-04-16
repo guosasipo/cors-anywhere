da17b9d5328cafdbff8d785b4dccd8c399b3db52
Lee HoJoon <leehojoonlee0820@gmail.com>
2024-04-16 17:23:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   httpsOptions: {
-        key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+        key: fs.readFileSync('/app/test/key.pem'),
+        cert: fs.readFileSync('/app/test/cert.pem'),
       },
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
