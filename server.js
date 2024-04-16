e89b9b2c030e7b4d71b8ec3cd6affb381f66c9dd
Lee HoJoon <leehojoonlee0820@gmail.com>
2024-04-16 19:36:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var fs = require("fs");
 var host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
+// for https
+var port_https = process.env.PORT_https || 8080;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
